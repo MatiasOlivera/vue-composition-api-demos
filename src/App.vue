734517a8c26ea93699e5f5dca2341b99635bafc2
Matías Olivera <matiasolivera@outlook.com>
2020-02-27 19:24:27 -0300

    feat: add counter demo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link :to="{ name: 'Counter' }">Counter</router-link>
     </div>
     <router-view />
   </div>
